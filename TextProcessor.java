05c3f4770a2e76eed682952ac34bc57b8583ecdf
Emily Samuel <emisam26@bergen.org>
2026-01-06 09:04:53 -0500

    1/5 CW

--- TextProcessor.java
+++ TextProcessor.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 public class TextProcessor {
     private ArrayList<String> textList;   // The list of text from the file
     /*
      * Constructor to create a TextProcessor with the specified filename
      */
     public TextProcessor(ArrayList<String> lines) {
-        setTextList(lines);
+      setTextList(lines);
     }
     /*
     * Changes the textList to the newTextList
@@ -16,11 +18,36 @@ public class TextProcessor {
     /*
    * Finds and removes all occurrences of each stop word from textList
    */
-/*
-   * Finds and removes all occurrences of each stop word from textList
-   */
   public void removeStopWords(ArrayList<String> stopWords) {
+    /*ArrayList<String> removeStop = new ArrayList<>();
+    //itterate through lines
+    for (String text:textList){
+      String line = text;
+      //itterate through stop words
+      for (String word:stopWords){
+        if (text.contains(word)){
+          int index = text.indexOf(word);
+          int len = word.length;
+          line = line.substring(0,index) + line.substring(index+len);
+        }
+      }
+      //add to new list
+      removeStop.add(line);
+    }
+    System.out.println(removeStop);*/
 
+    for (String stop:stopWords){
+      for (int i=0; i<textList.size(); i++){
+        String line = textList.get(i);
+        ArrayList<String> words = FileOperator.getWords(line);
+        for (int j=0; j<words.size(); j++){
+          if (words.get(j).equals(stop)){
+            words.remove(j);
+            j--;
+          }
+        }
+      }
+    }
   }
    /*
    * Returns a String containing the text in textList
@@ -34,11 +61,6 @@ public class TextProcessor {
 
     return text;
   }
-
-
-
-
-
 
     public static void main(String[] args) {
         ArrayList<String> stopwords = FileOperator.getStringList("stopwords.txt");
